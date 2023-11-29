52fc0a90fc238ae40ebbfc3dcabad2dce6e40664
MsMLyons <ms.markilyons@gmail.com>
2023-11-28 19:33:52 -0500

    Update Home Introductory Message

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,15 +4,15 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 const Home = () => {
   return (
     
-    <div name='home' className='w-full h-screen bg-gradient-to-r from-[#020024]  to-[#00d4ff]'>
+    <div name='home' className='w-full h-screen bg-gradient-to-r from-[#020024]  to-[#00d5ff]'>
         {/* name property set to help with smooth scroll */}
 
         {/* Container */}
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
             <p className='text-[#00FFFF] py-1'>Hi, my name is</p>
             <h1 className='text-4xl sm:text-7xl font-bold text-[#e2e9fb] py-3'>Marki Lyons</h1>
-            <h2 className='text-3xl sm:text-6xl font-bold text-[#e2e9fb] py-2'>I'm a front-end developer.</h2>
-            <p className='text-[#bec2cd] py-1 max-w-[700px]'>I also have experience in full-stack 
+            <h2 className='text-3xl sm:text-6xl font-bold text-[#e2e9fb] py-2'>I'm a software developer</h2>
+            <p className='text-[#bec2cd] py-1 max-w-[700px]'>With experience in full-stack 
                 web applications.
             </p>
             <div>
